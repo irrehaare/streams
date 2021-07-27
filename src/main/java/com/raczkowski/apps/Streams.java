b6f31233c5042ffb4139f26a5ea1e4a6b78d0330
irrehaare <wjtkwilk@gmail.com>
2021-07-27 20:43:03 +0200

    Implement partitionAdults

--- src/main/java/com/raczkowski/apps/Streams.java
+++ src/main/java/com/raczkowski/apps/Streams.java
@@ -52,13 +52,8 @@ public class Streams {
 
     // Partition these people into adults and kids, you'll need a special collector for this one
     public static Map<Boolean, List<Person>> partitionAdults(List<Person> people) {
-        Map<Boolean, List<Person>> map = new HashMap<>();
-        map.put(true, new ArrayList<>());
-        map.put(false, new ArrayList<>());
-        for (Person person : people) {
-            map.get(person.getAge() >= 18).add(person);
-        }
-        return map;
+        return people.stream()
+                .collect(Collectors.groupingBy(person -> person.getAge() > 18));
     }
 
     // Group these people by nationality
