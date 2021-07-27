f46d42a628ce3d1be3fe16926128cfd41ffae84a
irrehaare <wjtkwilk@gmail.com>
2021-07-27 19:36:52 +0200

    Implement getOldestPerson

--- src/main/java/com/raczkowski/apps/Streams.java
+++ src/main/java/com/raczkowski/apps/Streams.java
@@ -30,13 +30,9 @@ public class Streams {
     }
 
     public static Person getOldestPerson(List<Person> people) {
-        Person oldestPerson = new Person("", 0);
-        for (Person person : people) {
-            if (person.getAge() > oldestPerson.getAge()) {
-                oldestPerson = person;
-            }
-        }
-        return oldestPerson;
+        return people.stream()
+                .max(Comparator.comparingInt(Person::getAge))
+                .orElseThrow();
     }
 
     public static int calculate(List<Integer> numbers) {
